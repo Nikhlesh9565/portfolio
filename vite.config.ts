b0aa2c511bb451cf2c61e15f5ac57be244f90c46
Nikhil Vishwakarma <nikhil@logicpro>
2025-06-29 14:32:13 +0530

    Update homepage content

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
       compressionOptions: { level: 9 }
     }),
     visualizer({
-      open: true,
+      open: false,
       gzipSize: true,
       brotliSize: true,
     })
